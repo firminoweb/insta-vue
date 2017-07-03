82999f52fa80f3f7a6d48c0a202757d8c94b5087
João Firmino <firminoata@gmail.com>
2017-07-02 23:29:13 -0300

    Update/Remove: Update /api resource cleaning comments

--- src/api/index.js
+++ src/api/index.js
@@ -9,39 +9,3 @@ export const HTTP = axios.create({
     'Content-Type': 'application/json'
   }
 })
-
-// END Points
-// const API_ROOT = {
-//   root: 'https://financeapi.zflow.com.br'
-// }
-
-// Exemple after exported
-
-// import { HTTP } from '../api';
-//
-// export default {
-//   data: () => ({
-//     carros: [],
-//     errors: []
-//   }),
-//
-//   listarCarros() {
-//     HTTP.get('carros')
-//     .then(response => {
-//       this.carros = response.data
-//     })
-//     .catch(e => {
-//       this.errors.push(e)
-//     })
-//   },
-//
-//   addCarros() {
-//     HTTP.post('/add/carros', {})
-//     .then((response) => {
-//       console.log(response)
-//     })
-//     .catch((error) => {
-//       console.log(error)
-//     })
-//   }
-// }
